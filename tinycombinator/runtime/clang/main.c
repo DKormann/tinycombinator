88cd80b118cf8ac71aa6d84e7b599d9f407ed5a8
DKormann <dominikormann@outlook.com>
2025-11-22 15:23:18 +0100

    test fixies

--- tinycombinator/runtime/clang/main.c
+++ tinycombinator/runtime/clang/main.c
@@ -212,7 +212,7 @@ void _tree(PORT port, int d, BST* seen, Runtime* rt){
       _tree(src[0], d+1, seen, rt);
       break;
     case Dup:{
-      printf("Dup%d %d xor%d %d\n", get_label(port, rt), get_side(port), src[1] >> 1, src[1] & 1);
+      printf("Dup%d %d xor %d,%d\n", get_label(port, rt), get_side(port), src[1] >> 1, src[1] & 1);
       _tree(src[0], d+1, seen, rt);
       break;
     }
@@ -293,8 +293,7 @@ PORT new_node(Tag tag, int label, Runtime* rt){
 
 void free_node(PORT port, Runtime* rt){
   if (get_tag(port, rt) == Freed){
-    printf(RED "Error: Term %s is already freed\n", term_fmt(port, rt));
-    exit(1);
+    return;
   }
   Term* term = get_term(port, rt);
   term->tag = Freed;
@@ -343,11 +342,15 @@ void _check(PORT owner, BST* seen,Runtime* rt){
     }
 
     case Dup:{
+      // if (term->s[1] == 0){
+      //   tree(rt);
+      //   error("DUP ERROR: aux port is 0", rt);
+      // }
       PORT other = term->s[1] ^ owner;
+
 
       if (get_term(other, rt)->tag != ERA && get_port(other, rt) >> 1 != port >> 1){
         tree(rt);
-
         printf(RED "DUP ERROR: %s\n" RESET, port_fmt(port, rt));
         printf("owner: %s\n", port_fmt(owner, rt));
         printf("other: %s\n", port_fmt(other, rt));
@@ -382,7 +385,6 @@ void check_term(Runtime* rt){
 
 void erase(PORT owner, Runtime* rt){
   PORT port = get_port(owner, rt);
-
   if (DEBUG>=2) printf("erase %d,%d -> %s\n", owner >> 1, owner &1, port_fmt(port, rt));
   switch (get_tag(port, rt)){
     case Lam:{
@@ -407,9 +409,10 @@ void erase(PORT owner, Runtime* rt){
     }
     case App:
     case Sup:{
-      free_node(port, rt);
+      get_term(port, rt)->tag = Freed;
       erase(to_s0(port), rt);
       erase(to_s1(port), rt);
+      free_node(port, rt);
       break;
     }
     default: break;
@@ -626,13 +629,13 @@ int handle_redex(PORT owner, PORT term, Runtime* rt){
       ( otag == Lam ? app_lam
       : otag == Sup ? app_sup
       : otag == Null ? app_null
-      // : otag == Prim ? app_prim
+      : otag == Prim ? app_prim
       : NULL)
     :(tag == Dup) ?
       ( otag == Lam ? dup_lam
       : otag == Sup ? dup_sup
       : otag == Null ? dup_null
-      // : otag == Prim ? dup_prim
+      : otag == Prim ? dup_prim
       : NULL) : NULL;
 
   if (handler != NULL){
